bd1a98d164bd3f1713be45317ed0fb9dd1186c9c
Hafis Kalankomban <hafis3@gmail.com>
2021-06-19 23:49:35 +0530

    Router Added and Checkout componant initialized

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import "./App.css";
 import Header from "./Componants/Header/Header";
 import Home from "./Componants/Home/Home";
+import Checkout from "./Componants/Checkout/Checkout";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <Home />
-    </div>
+    <Router>
+      <div className="App">
+        <Header />
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/checkout">
+            <Checkout />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/Componants/Checkout/Checkout.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Checkout() {
+  return (
+    <div className="checkout">
+      <div className="checkout__left">
+        <img src="" alt="" className="checkOut__ad" />
+        <div>
+          <h2 className="checkout__title">Your Shopping Baskter</h2>
+        </div>
+      </div>
+      <div className="checkout__right"></div>
+    </div>
+  );
+}
+
+export default Checkout;
